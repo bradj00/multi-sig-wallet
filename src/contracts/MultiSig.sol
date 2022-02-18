146fe2be4fb30f9346249a2b6aaed7aeee66e466
bradj00 <brjohnson00@gmail.com>
2022-02-18 13:54:38 -0600

    Update MultiSig.sol

--- src/contracts/MultiSig.sol
+++ src/contracts/MultiSig.sol
@@ -2,16 +2,41 @@ pragma solidity 0.7.6;
 pragma abicoder v2;
 
 contract MultiSig {
+    address contractOwner = 0x9A3A8Db1c09cE2771A5e170a01a2A3eFB93ADA17;
+    address[] public approvers = [
+        0xF9108C5B2B8Ca420326cBdC91D27c075ea60B749,
+        0x7ab8a8dC4A602fAe3342697a762be22BB2e46d4d,
+        0x9A3A8Db1c09cE2771A5e170a01a2A3eFB93ADA17
+    ];
+
     event Deposit(address indexed fromThisGuy, uint valueGuy);
-    event alertNewApproval(address indexed fromGuy, address sendToGuy, string  reasonGuy, uint amountGuy);
+    event alertNewApproval(address indexed fromGuy, address sendToGuy, string  reasonGuy, uint amountGuy, uint idGuy);
     
-    
-    uint256 contractBalance = address(this).balance;
-    address[] public approvers;
     uint thresholdForApprovalToPass;
+    uint256 contractBalance = address(this).balance;
+    
+ 
+    struct Custodian {
+        address thisAddress;
+        uint voteWeight;
+    }
+    Custodian[] custodians;
 
-  //mapping(address => mapping(uint => bool) ) approvals;
-
+    function getContractOwner() public view returns (address) {
+        return(contractOwner);
+    }
+    
+    function firstRun() public returns(string memory){
+        if (msg.sender != contractOwner){
+            return('only contract owner can run this function.');
+        }else {
+        for (uint256 i = 0; i < approvers.length; i++) {
+            Custodian memory newRequest = Custodian(approvers[i], 1);
+            custodians.push(newRequest);
+        }
+        return('succeeded in setting approvers and vote weight!');
+        }
+    }
 
     struct Requests {
         address receipient;
@@ -30,7 +55,7 @@ contract MultiSig {
     function newApproval(address _sendTo, string memory _reason, uint _amount) public {
         Requests memory newRequest = Requests(_sendTo, transferRequests.length, _amount, _reason);
         transferRequests.push(newRequest);
-        emit alertNewApproval(msg.sender, _sendTo, _reason, _amount);
+        emit alertNewApproval(msg.sender, _sendTo, _reason, _amount, transferRequests.length);
     }
 
 
@@ -50,7 +75,10 @@ contract MultiSig {
 
     }
 
-
+    function getCustodians() public view returns (Custodian [] memory){
+        return(custodians);
+    }
+    
 
 
 }
